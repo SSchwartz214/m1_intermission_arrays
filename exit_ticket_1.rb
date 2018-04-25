3c674cd9ba246fadae5ff52e626dcc78fd09f6ed
Seth Schwartz <sschwartz214@gmail.com>
2018-04-25 10:12:46 -0600

    completed work

--- exit_ticket_1.rb
+++ exit_ticket_1.rb
@@ -1,12 +1,24 @@
 # TO DO:
 # Create an array of six elements - content of your choosing (either integers or strings!)
+teams = ["Knicks", "Warriors", "Wizards"]
 # Access the third element
+teams[2]
 # Add an element to the end of your array
+teams << "Heat" || teams.push("Heat")
 # Add an element to index 2
+teams.insert(2, "Spurs")
 # Remove the first element from your array
+teams.shift || teams.delete_at(0)
 # Remove any duplicates that may exist in your array
+teams.uniq
 # Create a second array with 3 elements
+players = "Kistaps Porzingas", "Steph Curry", "John Wall"
+
 # "Sqash" the two arrays together
+teams + players || teams.concat(players) || teams | players
 
 # BONUS since we didn't cover today 🦄
-# If your array contains strings, write a sentence using each string. If it contains integers, create a multiplication table for your array.
+# If your array contains strings, write a sentence using each string. If it contains integers, create a multiplication table for your array.\
+a = teams.join(', ')
+b = players.join(', ')
+"#{a} are teams in the NBA and #{b} are players in the NBA."
--- exit_ticket_2.rb
+++ exit_ticket_2.rb
@@ -17,24 +17,69 @@ hedgies = ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Margo", "Murphy", "Pino
 new_hedgies = ["Mooshi", "Monkey"]
 
 hedgies[3]
+# returns the element at the 3rd index of the hedgies array as a which will be "Ernie".
+# this does not mutate the array
+# Return value: "Ernie"
 hedgies.empty?
-hedgies.at[5]
+# Evaluates the array to see if it is empty and will return true or false.
+# Return value: false
+hedgies.at(5)
+# Returns the element at the specified index (whats in the parenthesis).
+# Can also use a negative number to start from end.
+# Does not mutate
+# Return value: "Margo"
 hedgies.unshift("Bella")
+# adds an element to the begging of the array
+# mutates
+# Return value: hedgies = ["Bella", "Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Margo", "Murphy", "Pino", "Spike", "Trix"]
 hedgies.insert(6, "Onyx")
+# inserts an element at the specified index
+# mutates
+# Return value: hedgies = ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Margo", "Onyx" "Murphy", "Pino", "Spike", "Trix"]
 hedgies.compact
+# Removes all instances of nil from array
+# Does not mutate (compact! does)
+# Return value: hedgies = ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Margo", "Murphy", "Pino", "Spike", "Trix"]
 hedgies.pop
+# Removes and returns the last element of the array. Can take an aruement
+# which will remove specified number of elements starting with last
+# mutates
+# Return value: "Trix"
 hedgies.fetch(0)
+# Returns the element of the specified index
+# Does not mutate
+# Return value: "Alvin"
 hedgies.include?("Bagel")
+# Checks to see if the array includes the specified element.
+# Returns true/false
+# Return value: false
 hedgies.shift
+# Removes first element of array and returns it.  This will "shift" entire
+# array to the left, changing each elements index.
+# Can take an arguement which will remove the specified number of elements,
+# starting with the first.
+# mutates
+# Return value: "Avlin"
 hedgies.push("Toast")
+# adds specified element to end of array.
+# mutates
+# Return value: hedgies = ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Margo", "Murphy", "Pino", "Spike", "Trix", "Toast"]
 hedgies << "Rocky"
+# Shovels in or adds item to end of array
+# mutates
+# Return value: hedgies = ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Margo", "Murphy", "Pino", "Spike", "Trix", "Rocky"]
 hedgies.count
-# also, what is at least one other way to accomplish this?
+# returns the amount of elements in array
+# Return value: 10
+# Other methods to accomplish this are .size and .length
 hedgies.uniq!
-# also, how would this be different if we omitted the "!"?
+# returns original array with duplicated elements removed.
+# .uniq creates a new array
 hedgies.concat(new_hedgies)
-
-# BONUS since we didn't cover today 🦄
+# Combines two arrays.  Mutates the array that the method is being called on but
+# not the array being passed in as an argument.
+# Return value: hedgies = ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Margo", "Murphy", "Pino", "Spike", "Trix", "Mooshi", "Monkey"]
 hedgies.each do |hedgie|
   puts hedgie
 end
+# iterates through the away and returns every item in the array as a separate string.
